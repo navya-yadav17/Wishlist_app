8ae185d48333082dec88fb18b7e26a4f6116ba2a
Navya Yadav <81849339+navya-yadav17@users.noreply.github.com>
2022-06-28 14:44:58 +0530

    Add files via upload

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+var wishlist = artifacts.require("wishlist");
+
+module.exports = function(deployer) {
+  deployer.deploy(wishlist);
+};
---
+++ test/wishlist.test.js
@@ -0,0 +1,44 @@
+const wislist = artifacts.require('./wislist.sol')
+
+contract('wislist', (accounts) => {
+  before(async () => {
+    this.wislist = await wislist.deployed()
+  })
+
+  it('deploys successfully', async () => {
+    const address = await this.wislist.address
+    assert.notEqual(address, 0x0)
+    assert.notEqual(address, '')
+    assert.notEqual(address, null)
+    assert.notEqual(address, undefined)
+  })
+
+  it('lists tasks', async () => {
+    const taskCount = await this.wislist.taskCount()
+    const task = await this.wislist.tasks(taskCount)
+    assert.equal(task.id.toNumber(), taskCount.toNumber())
+    assert.equal(task.content, 'Check out dappuniversity.com')
+    assert.equal(task.completed, false)
+    assert.equal(taskCount.toNumber(), 1)
+  })
+
+  it('creates tasks', async () => {
+    const result = await this.wislist.createTask('A new task')
+    const taskCount = await this.wislist.taskCount()
+    assert.equal(taskCount, 2)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 2)
+    assert.equal(event.content, 'A new task')
+    assert.equal(event.completed, false)
+  })
+
+  it('toggles task completion', async () => {
+    const result = await this.wislist.toggleCompleted(1)
+    const task = await this.wislist.tasks(1)
+    assert.equal(task.completed, true)
+    const event = result.logs[0].args
+    assert.equal(event.id.toNumber(), 1)
+    assert.equal(event.completed, true)
+  })
+
+})
